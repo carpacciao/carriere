0e1a1b227890c8ca8036a59c41097686e07b8743
carpacciao <43958708+carpacciao@users.noreply.github.com>
2024-05-04 12:00:53 +0200

    feat: add core package

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "core",
+      fileName: (format) => `core.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
